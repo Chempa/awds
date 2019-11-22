1ecf6f51f2239261e3976823ecfb903bfa62a6f0
Chempa <30638759+Chempa@users.noreply.github.com>
2019-11-22 17:25:41 +0000

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","awds");
+$con = mysqli_connect("remotemysql.com","LL6loZi4nz","zUykLmTuPw","LL6loZi4nz");
 // Check connection
 if (mysqli_connect_errno())
   {
